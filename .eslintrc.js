8f582db084285efd238a2482166f9d9101552590
unknown <qatheorys@outlook.com>
2020-07-18 15:43:49 +0700

    update indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: ['prettier'],
-  // add your custom rules here
-  rules: {},
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        parser: "babel-eslint",
+    },
+    extends: [
+        "@nuxtjs",
+        "prettier",
+        "prettier/vue",
+        "plugin:prettier/recommended",
+        "plugin:nuxt/recommended",
+    ],
+    plugins: ["prettier"],
+    "prettier/prettier": [
+        "error",
+        {
+            endOfLine: "auto",
+        },
+    ],
+    // add your custom rules here
+    rules: {},
 }
