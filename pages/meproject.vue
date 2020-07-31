4adff392aa9bf0fdb5111d259e6e897d5ed5b6a9
Keep Silent <qatheorys@outlook.com>
2020-07-31 17:34:51 +0700

    update UI

--- pages/meproject.vue
+++ pages/meproject.vue
@@ -1,15 +1,68 @@
 <template>
     <v-layout>
-        <v-flex class="text-center">
-            <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote class="blockquote">
-                &#8220;Trang web đang trong quá trình xây dựng.&#8221;
-                <footer>
-                    <small>
-                        <em>&mdash;Keep</em>
-                    </small>
-                </footer>
-            </blockquote>
+        <v-flex>
+            <v-card>
+                <v-toolbar color="indigo" dark>
+                    <v-toolbar-title
+                        >Nhập yêu cầu của hộp giảm tốc</v-toolbar-title
+                    >
+
+                    <v-spacer></v-spacer>
+
+                    <!-- <v-btn icon>
+                        <v-icon>mdi-magnify</v-icon>
+                    </v-btn> -->
+                </v-toolbar>
+                <v-card-text>
+                    <v-text-field
+                        v-model="ConveyorForce"
+                        label="Lực vòng trên băng tải F (kN)"
+                        required
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        v-model="conveyorSpeed"
+                        label="Tốc độ băng tải v (m/ph)"
+                        required
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        v-model="workingTime.hours"
+                        label="Số giờ làm việc"
+                        required
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        v-model="workingTime.shifts"
+                        label="Số ca làm việc"
+                        required
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        v-model="workingTime.days"
+                        label="Số ngày làm việc"
+                        required
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        v-model="workingTime.months"
+                        label="Số tháng làm việc"
+                        required
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        v-model="workingTime.years"
+                        label="Số năm làm việc"
+                        required
+                    >
+                    </v-text-field>
+                    <v-select
+                        v-model="turningDirection"
+                        :items="[1, 2]"
+                        label="Số chiều quay"
+                    ></v-select>
+                </v-card-text>
+            </v-card>
         </v-flex>
     </v-layout>
 </template>
